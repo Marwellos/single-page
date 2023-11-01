edc07e86d079d733c649a2e50c46eb808609e08a
Marwellos <marwellkozler@gmail.com>
2023-11-01 21:49:36 +0100

    new form on mysql

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const ejs = require('ejs'); // Import šablony EJS pro renderování HTML
-
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Importuj modul mysql2
+const mysql = require('mysql2');
+
+// Vytvoř připojení k MySQL databázi
+const db = mysql.createConnection({
+  host: 'localhost', // Název nebo IP adresa serveru MySQL
+  user: 'root', // Uživatelské jméno pro přihlášení k databázi
+  password: 'martinkozler66', // Heslo pro přihlášení k databázi
+  database: 'my_database', // Název databáze
+});
+
+// Připojení k databázi
+db.connect((err) => {
+  if (err) {
+    console.error('Chyba při připojování k databázi: ' + err.message);
+  } else {
+    console.log('Připojení k databázi bylo úspěšné');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -24,7 +43,21 @@ app.get('/formular', (req, res) => {
   res.render('formular');
 });
 
+app.post('/odeslat', (req, res) => {
+  const { name, email, phone, otherMedia, radioResult } = req.body;
 
+  // Zde můžeš provést SQL dotaz pro vložení dat do databáze
+  const sql = 'INSERT INTO kontaktni_formular (jmeno_prijmeni, email, telefon, co_s_autem, zpusob_kontaktu) VALUES (?, ?, ?, ?, ?)';
+  db.query(sql, [name, email, phone, otherMedia, radioResult], (err, result) => {
+    if (err) {
+      console.error('Chyba při vkládání dat do databáze: ' + err.message);
+      res.redirect('/chyba'); // Pokud dojde k chybě, můžeš přesměrovat na stránku s chybou
+    } else {
+      console.log('Data byla úspěšně uložena do databáze');
+      res.redirect('/home'); // Pokud vše proběhne v pořádku, přesměruj na stránku s úspěšným odesláním formuláře
+    }
+  });
+});
 
 //git add -A
 //git commit -m "co jsem udělal"
